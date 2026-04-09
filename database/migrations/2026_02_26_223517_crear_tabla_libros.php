d4ca6b7b95154172d94c00c83e224a8c973dc2bb
Angeelvr2 <l22260109@matamoros.tecnm.mx>
2026-04-08 19:25:02 -0500

    categorias

--- database/migrations/2026_02_26_223517_crear_tabla_libros.php
+++ database/migrations/2026_02_26_223517_crear_tabla_libros.php
@@ -8,18 +8,12 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('categorias', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre',255);
-            $table->timestamps();
-        });
-
         Schema::create('libros', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('nombre', 250);
             $table->string('isbn', 100);
-            $table->string('autor',255);
+            $table->string('autor', 255);
             $table->string('editorial', 250);
             $table->smallInteger('estatus')->default(0);
             $table->unsignedBigInteger('id_categoria');
@@ -29,12 +23,8 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('categorias');
         Schema::dropIfExists('libros');
     }
 };
